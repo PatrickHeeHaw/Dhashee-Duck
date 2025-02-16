7d96976e69bb1d2352a8283cb1b318e0338a70f9
Patrick Ha <149280649+PatrickHeeHaw@users.noreply.github.com>
2025-02-16 00:07:11 -0600

    Add files via upload

--- lib/StatefulWidget.dart
+++ lib/StatefulWidget.dart
@@ -1,38 +1,22 @@
-
 import 'package:flutter/material.dart';
 
 class DunkWidget extends StatelessWidget {
-  const DunkWidget({Key? key}) : super(key: key);
+  const DunkWidget({super.key});
 
-  _onPressed() {
-    print('Dunk Contest');
+  onPressed() {
+    print('Dahsee Duck');
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.yellow,
-      child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Center(
-                    ),
-                    Image(image: AssetImage("assets/Duck.png"), width: 100, height: 100),
-                  ],
-                ),
-              ),
-              Text('Contest'),
-              ElevatedButton(
-                onPressed: _onPressed,
-                child: Text('Enter'),
-              ),
-            ],
-      
+      color: const Color.fromARGB(255, 161, 232, 244),
+      child: Center(
+        child: Image(
+          image: AssetImage("assets/Duck.png"),
+          width: 500,
+          height: 500,
+        ),
       ),
     );
   }
